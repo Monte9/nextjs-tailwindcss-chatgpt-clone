1b5f1a1f0663f0632afbbb54dbd5910f84168e9c
Shreyas <sparab22@alumni.stanford.edu>
2023-07-05 08:55:50 -0700

    help

--- next.config.js
+++ next.config.js
@@ -3,7 +3,6 @@ const nextConfig = {
   reactStrictMode: false,
   env: {
     OPENAI_API_KEY: process.env.OPENAI_API_KEY,
-    MIXPANEL_PROJECT_TOKEN: process.env.MIXPANEL_PROJECT_TOKEN,
     APP_ENV: process.env.APP_ENV,
     APP_NAME: process.env.APP_NAME
   }
