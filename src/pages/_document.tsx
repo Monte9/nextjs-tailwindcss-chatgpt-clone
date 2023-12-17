54ffe27059d293f2f474856bb6e639e99b1bc67c
Willian Agostini <willian.agostini@gmail.com>
2023-12-16 23:24:14 -0300

    feat: add SpeedInsights

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function Document() {
   return (
@@ -7,6 +8,7 @@ export default function Document() {
       <body className="bg-background">
         <Main />
         <NextScript />
+        <SpeedInsights />
       </body>
     </Html>
   );
